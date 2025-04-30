c272982186ae0a3b1629687f75d00c8e88e6a071
José Guilherme <guisant@icloud.com>
2025-04-30 13:40:26 -0300

    finalizando sidebar

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -1,19 +1,21 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css'
 export function Sidebar(){
     return(
         <aside className={styles.sidebar}>
             <img 
                 className={styles.cover} src="https://images.unsplash.com/photo-1599507593499-a3f7d7d97667?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                alt="" 
             />
             
             <div className={styles.profile}>
-                <strong>Guilherme Santos</strong>
+                <img className={styles.avatar} src="https://github.com/Guilherme-Santt.png" />
+                <strong>José Guilherme</strong>
                 <span>Web Developer</span>
             </div>
-            
+
             <footer>
                 <a href="">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
